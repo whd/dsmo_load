7e882015d364e0c078403bfe62f86e12280ac671
Blake Imsland <blake@retroco.de>
2016-11-07 17:54:52 -0800

    Clean up configs for use with new hindsight

--- hindsight/hs_run/output/dsmo_errors_redshift.lua
+++ hindsight/hs_run/output/dsmo_errors_redshift.lua
@@ -2,7 +2,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-local ds = require "derived_stream"
+local ds = require "heka.derived_stream"
 
 local name = read_config("table_prefix") or "download_stats_errors"
 local schema = {
